e17870f600ecf218c7c87c3e6df82751beeba22a
zhpadvoiski <zh.padvoiski@gmail.com>
2021-09-30 14:04:37 -0500

    tests added

--- src/utils/tests/tree.test.js
+++ src/utils/tests/tree.test.js
@@ -1,11 +1,15 @@
 import { NewNode, NewTree } from "../tree";
 
-describe("tree", () => {
+describe("node", () => {
   describe("NewNode class", () => {
     const node = new NewNode();
     test("should be instance of NewNode class", () => {
       const mockResult = node instanceof NewNode;
       expect(mockResult).toBeTruthy();
+    });
+    test("should have link to the new node in children", () => {
+      node.children[0] = new NewNode(9);
+      expect(node.children[0].value).toBe(9);
     });
   });
 });
