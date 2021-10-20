287ee2aa54103f39b93f4398d2cfee882fdbe9a7
zhpadvoiski <zh.padvoiski@gmail.com>
2021-10-19 20:57:42 -0500

    tree bugs fixed

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -14,37 +14,3 @@ let b = {
 
 console.log("a:", a);
 console.log("b:", b);
-
-function A() {
-	this.number = 10;
-	this.show = () => {
-		console.log(this.number);
-	};
-}
-let a = new A();
-let b = {
-	number: 20,
-	show: () => {
-		console.log(this.number);
-	},
-};
-
-console.log("a:", a);
-console.log("b:", b);
-
-function A() {
-	this.number = 10;
-	this.show = () => {
-		console.log(this.number);
-	};
-}
-let a = new A();
-let b = {
-	number: 20,
-	show: () => {
-		console.log(this.number);
-	},
-};
-
-console.log("a:", a);
-console.log("b:", b);
--- src/utils/tree.js
+++ src/utils/tree.js
@@ -1,165 +1,166 @@
 function Node(value, left, right) {
-  this.value = value === undefined ? null : value;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
+	this.value = value === undefined ? null : value;
+	this.left = left === undefined ? null : left;
+	this.right = right === undefined ? null : right;
 }
 
 export default class Tree {
-  constructor() {
-    this.root = null;
-  }
+	constructor() {
+		this.root = null;
+	}
 
-  add(value) {
-    let node = this.root;
-    if (!node) {
-      this.root = new Node(value);
-      return;
-    }
+	add(value) {
+		let node = this.root;
+		if (!node) {
+			this.root = new Node(value);
+			return;
+		}
 
-    search(node);
+		search(node);
 
-    function search(node) {
-      if (value < node.value) {
-        if (!node.left) {
-          node.left = new Node(value);
-          return;
-        } else {
-          return search(node.left);
-        }
-      }
-      if (value > node.value) {
-        if (!node.right) {
-          node.right = new Node(value);
-          return;
-        } else {
-          return search(node.right);
-        }
-      }
-      return;
-    }
-  }
+		function search(node) {
+			if (value < node.value) {
+				if (!node.left) {
+					node.left = new Node(value);
+					return;
+				} else {
+					return search(node.left);
+				}
+			}
+			if (value > node.value) {
+				if (!node.right) {
+					node.right = new Node(value);
+					return;
+				} else {
+					return search(node.right);
+				}
+			}
+			return;
+		}
+	}
 }
 
 export function TreeNode(value) {
-  this.value = value === undefined ? "no value" : value;
-  this.x = 0;
-  this.y = 0;
-  this.prevSibling = null;
-  this.children = [];
+	this.value = value === undefined ? "no value" : value;
+	this.x = 0;
+	this.y = 0;
+	this.prevSibling = null;
+	this.left = null;
+	this.right = null;
 }
 
 export class NewTree {
-  constructor() {
-    this.root = null;
-  }
+	constructor() {
+		this.root = null;
+	}
 
-  add(value) {
-    let node = this.root;
-    if (!node) {
-      this.root = new TreeNode(value);
-      return;
-    }
+	add(value) {
+		let node = this.root;
+		if (!node) {
+			this.root = new TreeNode(value);
+			return;
+		}
 
-    search(node);
+		search(node);
 
-    function search(node) {
-      if (value < node.value) {
-        if (!node.children[0]) {
-          node.children[0] = new TreeNode(value);
-          return;
-        } else {
-          return search(node.children[0]);
-        }
-      }
-      if (value > node.value) {
-        if (!node.children[1]) {
-          node.children[1] = new TreeNode(value);
-          return;
-        } else {
-          return search(node.children[1]);
-        }
-      }
-      return;
-    }
-  }
+		function search(node) {
+			if (value < node.value) {
+				if (!node.left) {
+					node.left = new TreeNode(value);
+					return;
+				} else {
+					return search(node.left);
+				}
+			}
+			if (value > node.value) {
+				if (!node.right) {
+					node.right = new TreeNode(value);
+					return;
+				} else {
+					return search(node.right);
+				}
+			}
+			return;
+		}
+	}
 }
 
 const NODE_STEP = 400;
 export class TreeRenderer {
-  constructor(
-    dataRoot,
-    width = window.innerWidth,
-    height = window.innerHeight
-  ) {
-    //root node of the tree (entry point)
-    this.dataRoot = dataRoot;
-    this.width = width;
-    this.height = height;
-    this.renderRoot = this.prepareData(this.dataRoot, 0, null);
-    this.firstPass(this.renderRoot);
-    this.deep = this.getDeep();
-  }
+	constructor(
+		dataRoot,
+		width = window.innerWidth,
+		height = window.innerHeight
+	) {
+		//root node of the tree (entry point)
+		this.dataRoot = dataRoot;
+		this.width = width;
+		this.height = height;
+		this.renderRoot = this.prepareData(this.dataRoot, 0, null);
+		this.firstPass(this.renderRoot);
+		this.deep = this.getDeep();
+	}
 
-  /*
-   * Build an intermediate form of the original data tree.  The nodes of
-   * this new tree will be instances of the TreeNode class.
-   */
-  getDeep() {
-    let deep = -Infinity;
+	/*
+	 * Build an intermediate form of the original data tree.  The nodes of
+	 * this new tree will be instances of the TreeNode class.
+	 */
+	getDeep() {
+		let deep = -Infinity;
 
-    helper(this.renderRoot, 0);
-    return deep;
+		helper(this.renderRoot, 0);
+		return deep;
 
-    function helper(node, level) {
-      deep = Math.max(deep, level);
-      for (let i = 0; i < node.children.length; i++) {
-        helper(node.children[i], level + 1);
-      }
-    }
-  }
-  prepareData(node, level, prevSibling) {
-    let treeNode = new TreeNode(node.value);
-    treeNode.y = level;
-    treeNode.prevSibling = prevSibling;
+		function helper(node, level) {
+			deep = Math.max(deep, level);
+			for (let i = 0; i < node.children.length; i++) {
+				helper(node.children[i], level + 1);
+			}
+		}
+	}
+	prepareData(node, level, prevSibling) {
+		let treeNode = new TreeNode(node.value);
+		treeNode.y = level;
+		treeNode.prevSibling = prevSibling;
 
-    for (let i = 0; i < node.children.length; i++) {
-      if (node.children[i]) {
-        treeNode.children.push(
-          this.prepareData(
-            node.children[i],
-            level + 200,
-            i >= 1 ? treeNode.children[i - 1] : null
-          )
-        );
-      }
-    }
-    return treeNode;
-  }
+		for (let i = 0; i < node.children.length; i++) {
+			if (node.children[i]) {
+				treeNode.children.push(
+					this.prepareData(
+						node.children[i],
+						level + 200,
+						i >= 1 ? treeNode.children[i - 1] : null
+					)
+				);
+			}
+		}
+		return treeNode;
+	}
 
-  firstPass(node) {
-    for (let i = 0; i < node.children.length; i++) {
-      this.firstPass(node.children[i]);
-    }
+	firstPass(node) {
+		for (let i = 0; i < node.children.length; i++) {
+			this.firstPass(node.children[i]);
+		}
 
-    node.x = node.prevSibling ? node.prevSibling.x + NODE_STEP : 0;
+		node.x = node.prevSibling ? node.prevSibling.x + NODE_STEP : 0;
 
-    if (node.children.length === 1) {
-      node.modifier = node.x;
-    } else if (node.children.length >= 2) {
-      let minX = Infinity;
-      let maxX = -minX;
-      for (let i = 0; i < node.children.length; i++) {
-        minX = Math.min(node.children[i].x, minX);
-        maxX = Math.max(node.children[i].x, maxX);
-      }
-      node.modifier = node.x - (maxX - minX) / 2;
-    }
-  }
+		if (node.children.length === 1) {
+			node.modifier = node.x;
+		} else if (node.children.length >= 2) {
+			let minX = Infinity;
+			let maxX = -minX;
+			for (let i = 0; i < node.children.length; i++) {
+				minX = Math.min(node.children[i].x, minX);
+				maxX = Math.max(node.children[i].x, maxX);
+			}
+			node.modifier = node.x - (maxX - minX) / 2;
+		}
+	}
 
-  secondPass(node, modSum) {
-    node.final = node.x + modSum;
-    for (let i = 0; i < node.children.length; i++) {
-      this.secondPass(node.children[i], node.modifier + modSum);
-    }
-  }
+	secondPass(node, modSum) {
+		node.final = node.x + modSum;
+		for (let i = 0; i < node.children.length; i++) {
+			this.secondPass(node.children[i], node.modifier + modSum);
+		}
+	}
 }
